049b162671db5708de94ef1b0b9b09105b62e050
andre-savedra <andre_savedra@hotmail.com>
2025-10-14 09:55:06 -0300

    removing unnecessary projects

--- appdinamico/android/app/src/main/kotlin/com/teste/appdinamico/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.teste.appdinamico
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_application_1/android/app/src/main/kotlin/com/teste/flutter_application_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.teste.flutter_application_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
