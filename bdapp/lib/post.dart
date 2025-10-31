0a8bbf9319750cae9deb891516441ea8fc8e4954
Viniciusalmei <vihhalmeida12@gmail.com>
2025-10-31 10:41:42 -0300

    post

---
+++ bdapp/lib/post.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class PostPage extends StatefulWidget {
+  const PostPage({super.key});
+
+  @override
+  State<PostPage> createState() => _PostPageState();
+}
+
+class _PostPageState extends State<PostPage> {
+  TextEditingController temperature =  TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    postValue();
+
+  }
+  
+  Future<void> postValue() async {
+    try {
+      FirebaseFirestore.instance.collection("monitoramento").add(
+        {
+          "temperatura": temperature.text,
+        }
+      );
+    }catch(e){
+      print("Erro ao enviar $e");
+    }
+    
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(children: [
+          TextField(
+            controller: temperature,
+          ),
+          ElevatedButton(onPressed: () {
+            postValue();
+          }, child: Text("Enviar")) 
+        ],),
+      ),
+    );
+  }
+}
